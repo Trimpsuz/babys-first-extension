5838360e0cc5d41a3a3cc7e8de589338968b7f19
alice <86567702+ihmemaassa@users.noreply.github.com>
2023-05-29 16:12:40 +0300

    Add files via upload

---
+++ time.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var AuthCodeLabel = browser.i18n.getMessage("AuthCodeLabel");
+  var ProjectNameLabel = browser.i18n.getMessage("ProjectNameLabel");
+  var Homepage = browser.i18n.getMessage("Homepage");
+  var ToggleLabel = browser.i18n.getMessage("ToggleLabel")
+  
+  var ToggleLabelElement = document.getElementById('toggle-label');
+  ToggleLabelElement.textContent = ToggleLabel;
+
+  var AuthCodeLabelElement = document.getElementById('auth-code-label');
+  AuthCodeLabelElement.textContent = AuthCodeLabel;
+
+  var ProjectNameLabelElement = document.getElementById('project-name-label');
+  ProjectNameLabelElement.textContent = ProjectNameLabel;
+
+  var HomepageElement = document.getElementById('Homepage');
+  HomepageElement.textContent = Homepage;
+
+  var form = document.getElementById('time-form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var authCode = document.getElementById('auth-code').value;
+    var projectName = document.getElementById('project-name').value;
+
+    console.log('Auth Code:', authCode);
+    console.log('Project Name:', projectName);
+
+    form.reset();
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var StatusOff = browser.i18n.getMessage("StatusOff");
+  var StatusOn = browser.i18n.getMessage("StatusOn");
+
+  var statusText = document.getElementById('status-text');
+  var statusImage = document.getElementById('statusimg');
+  
+  var enabledCheckbox = document.getElementById('enabled');
+
+  enabledCheckbox.addEventListener('change', function() {
+    if (enabledCheckbox.checked) {
+      statusText.textContent = StatusOn;
+      statusImage.style.backgroundImage = 'url("assets/time-on.png")';
+      browser.browserAction.setIcon({ path: 'assets/toolbar-on.png' });
+    } else {
+      statusText.textContent = StatusOff;
+      statusImage.style.backgroundImage = 'url("assets/time-off.png")';
+      browser.browserAction.setIcon({ path: 'assets/toolbar-off.png' });
+    }
+  });
+
+  // Initialize the toggle state
+  enabledCheckbox.checked = false;
+});
+
+
