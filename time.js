023ca50c0317070ad7d78f462fa3864f3d3bec02
Trimpsuz <mail@trimpsuz.xyz>
2023-06-04 23:46:51 +0300

    Turn extension on by default

--- time.js
+++ time.js
@@ -1,54 +1,55 @@
 //tämä osa hoitaa tekstien käännökset
-document.addEventListener('DOMContentLoaded', function () {
-  var AuthCodeLabel = browser.i18n.getMessage('AuthCodeLabel');
-  var Homepage = browser.i18n.getMessage('Homepage');
-  var ToggleLabel = browser.i18n.getMessage('ToggleLabel');
+var AuthCodeLabel = browser.i18n.getMessage('AuthCodeLabel');
+var Homepage = browser.i18n.getMessage('Homepage');
+var ToggleLabel = browser.i18n.getMessage('ToggleLabel');
 
-  var ToggleLabelElement = document.getElementById('toggle-label');
-  ToggleLabelElement.textContent = ToggleLabel;
+var ToggleLabelElement = document.getElementById('toggle-label');
+ToggleLabelElement.textContent = ToggleLabel;
 
-  var AuthCodeLabelElement = document.getElementById('auth-code-label');
-  AuthCodeLabelElement.textContent = AuthCodeLabel;
+var AuthCodeLabelElement = document.getElementById('auth-code-label');
+AuthCodeLabelElement.textContent = AuthCodeLabel;
 
-  var HomepageElement = document.getElementById('Homepage');
-  HomepageElement.textContent = Homepage;
+var HomepageElement = document.getElementById('Homepage');
+HomepageElement.textContent = Homepage;
 
-  var form = document.getElementById('time-form'); //ei aavistustakaan mikä tää 'time-form' on ja miksi tää toimii, en uskalla koskea ettei kaikki hajoa
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
+var form = document.getElementById('time-form'); //ei aavistustakaan mikä tää 'time-form' on ja miksi tää toimii, en uskalla koskea ettei kaikki hajoa
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-    var authCode = document.getElementById('auth-code').value;
+  var authCode = document.getElementById('auth-code').value;
 
-    browser.storage.local.set({ authCode: authCode }).then(() => {
-      console.log('Authcode set in local storage.');
-    });
-
-    form.reset();
+  browser.storage.local.set({ authCode: authCode }).then(() => {
+    console.log('Authcode set in local storage.');
   });
+
+  form.reset();
 });
+
+var StatusOff = browser.i18n.getMessage('StatusOff');
+var StatusOn = browser.i18n.getMessage('StatusOn');
+
+var statusText = document.getElementById('status-text');
+var statusImage = document.getElementById('statusimg');
+
+var enabledCheckbox = document.getElementById('enabled');
+
+//Set on by default
+statusText.textContent = StatusOn;
+statusImage.style.backgroundImage = 'url("assets/time-on.png")';
+browser.browserAction.setIcon({ path: 'assets/toolbar-on.png' });
 
 //tää osa vaihtaa kuvia ja tekstejä on/off
-document.addEventListener('DOMContentLoaded', function () {
-  var StatusOff = browser.i18n.getMessage('StatusOff');
-  var StatusOn = browser.i18n.getMessage('StatusOn');
-
-  var statusText = document.getElementById('status-text');
-  var statusImage = document.getElementById('statusimg');
-
-  var enabledCheckbox = document.getElementById('enabled');
-
-  enabledCheckbox.addEventListener('change', function () {
-    if (enabledCheckbox.checked) {
-      statusText.textContent = StatusOn;
-      statusImage.style.backgroundImage = 'url("assets/time-on.png")';
-      browser.browserAction.setIcon({ path: 'assets/toolbar-on.png' });
-    } else {
-      statusText.textContent = StatusOff;
-      statusImage.style.backgroundImage = 'url("assets/time-off.png")';
-      browser.browserAction.setIcon({ path: 'assets/toolbar-off.png' });
-    }
-  });
-
-  // Initialize the toggle state
-  enabledCheckbox.checked = false;
+enabledCheckbox.addEventListener('change', function () {
+  if (enabledCheckbox.checked) {
+    statusText.textContent = StatusOn;
+    statusImage.style.backgroundImage = 'url("assets/time-on.png")';
+    browser.browserAction.setIcon({ path: 'assets/toolbar-on.png' });
+  } else {
+    statusText.textContent = StatusOff;
+    statusImage.style.backgroundImage = 'url("assets/time-off.png")';
+    browser.browserAction.setIcon({ path: 'assets/toolbar-off.png' });
+  }
 });
+
+// Initialize the toggle state
+enabledCheckbox.checked = true;
