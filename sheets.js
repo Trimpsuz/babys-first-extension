c3966bc539c9226bac1b4fa2c9629541829ba5fe
Trimpsuz <mail@trimpsuz.xyz>
2023-06-05 02:28:17 +0300

    Add check for extension enable/disable

--- sheets.js
+++ sheets.js
@@ -2,41 +2,46 @@ const projectName = document.title.replace(' - Google Sheets', '');
 
 let editorText = '';
 
-//Send heartbeat
-setInterval(() => {
-  if (document.getElementById('waffle-rich-text-editor').children[0]) {
-    if (!document.hidden && editorText != document.getElementById('waffle-rich-text-editor').children[0].textContent) {
-      editorText = document.getElementById('waffle-rich-text-editor').children[0].textContent;
+//Check if extension is enabled
+browser.storage.local.get('extensionStatus').then((item) => {
+  if (Object.entries(item).length === 0 || item.extensionStatus) {
+    //Send heartbeat
+    setInterval(() => {
+      if (document.getElementById('waffle-rich-text-editor').children[0]) {
+        if (!document.hidden && editorText != document.getElementById('waffle-rich-text-editor').children[0].textContent) {
+          editorText = document.getElementById('waffle-rich-text-editor').children[0].textContent;
 
-      const data = {
-        projectName,
-        language: 'Google Sheets',
-        editor_name: 'browser',
-        hostname: 'browser',
-      };
+          const data = {
+            projectName,
+            language: 'Google Sheets',
+            editor_name: 'browser',
+            hostname: 'browser',
+          };
 
+          browser.storage.local.get('authCode').then((item) => {
+            const authCode = item.authCode;
+            fetch('https://api.testaustime.fi/activity/update', data, {
+              Headers: {
+                Authorization: `Bearer ${authCode}`,
+              },
+            });
+          });
+          console.log('Heartbeat');
+        }
+      }
+    }, 30_000);
+
+    //Flush when closing window
+    window.onbeforeunload = () => {
       browser.storage.local.get('authCode').then((item) => {
         const authCode = item.authCode;
-        fetch('https://api.testaustime.fi/activity/update', data, {
+        fetch('https://api.testaustime.fi/activity/flush', '', {
           Headers: {
             Authorization: `Bearer ${authCode}`,
           },
         });
       });
-      console.log('Heartbeat');
-    }
+      console.log('Flushed');
+    };
   }
-}, 30_000);
-
-//Flush when closing window
-window.onbeforeunload = () => {
-  browser.storage.local.get('authCode').then((item) => {
-    const authCode = item.authCode;
-    fetch('https://api.testaustime.fi/activity/flush', '', {
-      Headers: {
-        Authorization: `Bearer ${authCode}`,
-      },
-    });
-  });
-  console.log('Flushed');
-};
+});
