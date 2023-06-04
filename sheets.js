d2c1e91e25a4f5c1f3a88bb0de0ce788724b0a03
Trimpsuz <mail@trimpsuz.xyz>
2023-06-05 02:08:45 +0300

    Add sending heartbeat from google sheets

--- sheets.js
+++ sheets.js
@@ -1,9 +1,29 @@
-document.body.style.border = '5px solid red';
-
 const projectName = document.title.replace(' - Google Sheets', '');
 
-console.log(projectName);
+let editorText = '';
 
-browser.storage.local.get('authCode').then((item) => {
-  console.log(item.authCode);
-});
+//Send heartbeat
+setInterval(() => {
+  if (document.getElementById('waffle-rich-text-editor').children[0]) {
+    if (!document.hidden && editorText != document.getElementById('waffle-rich-text-editor').children[0].textContent) {
+      editorText = document.getElementById('waffle-rich-text-editor').children[0].textContent;
+
+      const data = {
+        projectName,
+        language: 'Google Sheets',
+        editor_name: 'browser',
+        hostname: 'browser',
+      };
+
+      browser.storage.local.get('authCode').then((item) => {
+        const authCode = item.authCode;
+        fetch('https://api.testaustime.fi/activity/update', data, {
+          Headers: {
+            Authorization: `Bearer ${authCode}`,
+          },
+        });
+      });
+      console.log('Heartbeat');
+    }
+  }
+}, 30_000);
