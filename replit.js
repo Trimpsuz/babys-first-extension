0e46eda85334ace66266c83f43fc806d9553c5e5
Trimpsuz <mail@trimpsuz.xyz>
2023-06-05 21:24:09 +0300

    Get current active editor and language

--- replit.js
+++ replit.js
@@ -20,3 +20,40 @@ function waitForElement(selector, callback) {
 waitForElement('[data-cy="header-repl-title"]', (element) => {
   const projectName = element.textContent;
 });
+
+waitForElement('.node.active', (element) => {
+  //Get current editor and language
+  const fileList = document.querySelector('.root-node');
+  let currentFile = document.querySelector('.node.active');
+  let currentEditor = null;
+  document.querySelector(`[data-cy="workspace-cm-editor-${currentFile.textContent}"]`).childNodes[0].childNodes[0].childNodes.forEach((node) => {
+    if (node.classList.contains('cm-scroller')) {
+      node.childNodes.forEach((node) => {
+        if (node.classList.contains('cm-content')) {
+          currentEditor = node;
+        }
+      });
+    }
+  });
+  let currentLanguage = currentEditor.getAttribute('data-language') ? currentEditor.getAttribute('data-language') : '';
+
+  function handleMutations(mutationsList, observer) {
+    currentFile = document.querySelector('.node.active');
+
+    document.querySelector(`[data-cy="workspace-cm-editor-${currentFile.textContent}"]`).childNodes[0].childNodes[0].childNodes.forEach((node) => {
+      if (node.classList.contains('cm-scroller')) {
+        node.childNodes.forEach((node) => {
+          if (node.classList.contains('cm-content')) {
+            currentEditor = node;
+          }
+        });
+      }
+    });
+
+    currentLanguage = currentEditor.getAttribute('data-language') ? currentEditor.getAttribute('data-language') : '';
+  }
+
+  const observer = new MutationObserver(handleMutations);
+
+  observer.observe(fileList, { childList: true, subtree: true, attributes: true });
+});
