160a8e6f809d26f027b0369012055e436632c8b2
Trimpsuz <mail@trimpsuz.xyz>
2023-06-07 15:00:20 +0300

    Send heartbeat on first action

--- shadertoy.js
+++ shadertoy.js
@@ -1,5 +1,31 @@
 let editorChanged = false;
 
+function heartbeat() {
+  browser.storage.local.get('authCode').then((item) => {
+    const authCode = item.authCode;
+    const data = {
+      project_name: document.title,
+      language: 'GLSL',
+      editor_name: 'Shadertoy',
+      hostname: 'browser',
+    };
+    fetch('https://api.testaustime.fi/activity/update', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${authCode}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log('Heartbeat');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+}
+
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     if (mutation.type === 'childList') {
@@ -9,3 +35,16 @@ const observer = new MutationObserver((mutations) => {
 });
 
 observer.observe(document.querySelector('.CodeMirror-code'), { childList: true, subtree: true });
+
+function handleFirstAction() {
+  document.removeEventListener('click', handleFirstAction);
+  heartbeat();
+}
+
+//Check if extension is enabled
+browser.storage.local.get('extensionStatus').then((item) => {
+  if (Object.entries(item).length === 0 || item.extensionStatus) {
+    //Send heartbeat on first action
+    document.addEventListener('click', handleFirstAction);
+  }
+});
